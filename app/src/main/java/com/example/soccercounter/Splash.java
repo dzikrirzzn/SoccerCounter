92777eb4bac750ede9d387fd2c57a37ff4901d1e
dzikrirzzn <dzikrirazzan02@gmail.com>
2021-01-21 08:55:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/soccercounter/Splash.java
@@ -0,0 +1,26 @@
+package com.example.soccercounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+    private int waktu_splash=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(Splash.this,MainActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        },waktu_splash);
+    }
+}
